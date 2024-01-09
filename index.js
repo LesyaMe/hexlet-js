5ec296bebed6b98bab03dd402db664c7157426be
OlesyaM <olesya.dedlovskaya@gmail.com>
2024-01-09 18:31:55 -0500

    вынесена функция в отдельный файл, файлом для запуска установлен src/index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+import getBirthYear from './src/index.js';
 console.log("Hello, Hexlet!");
 import _ from "lodash";
-console.log(_.last(['one', 'two']));
-console.log(_.random(0, 5));
+// console.log(_.last(['one', 'two']));
+console.log('Сколько вам лет?');
+const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
